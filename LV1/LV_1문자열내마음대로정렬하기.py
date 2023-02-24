5680e70b8d5fc10b509585eef7bb8365647f602a
bakingeol <bakingeol@gmail.com>
2023-02-25 01:47:56 +0900

    programmers

--- LV1/LV_1문자열내마음대로정렬하기.py
+++ LV1/LV_1문자열내마음대로정렬하기.py
@@ -10,3 +10,7 @@ def solution(strings, n):
 solution:
 strings를 정렬된 산태에서 dictionary x 의 value를 뽑아낸 값을 기준으로 다시 정렬
 '''
+# 더 간단한 방법
+def strange_sort(strings, n):
+    '''strings의 문자열들을 n번째 글자를 기준으로 정렬해서 return하세요'''
+    return sorted(strings, key=lambda x: x[n])
