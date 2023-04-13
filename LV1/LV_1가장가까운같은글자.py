1f9878b331bc6e35065f5c9de338df07a17bc0e9
bakingeol <bakingeol@gmail.com>
2023-04-14 01:09:10 +0900

    programmers

---
+++ LV1/LV_1가장가까운같은글자.py
@@ -0,0 +1,11 @@
+#%%
+s = "banana"
+result = [-1, -1, -1, 2, 2, 2]
+# %%
+x = []
+answer = []
+for i,v in enumerate(s):
+    for index, value in enumerate(x):
+        if value == v:
+            
+    x.append(v)
